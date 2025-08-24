c6510eccac05e818a3da3831020609a085c49a26
GOLDER303 <91141714+GOLDER303@users.noreply.github.com>
2025-08-24 15:57:45 +0200

    feat: create prisma service
    * feat: create prisma service
    
    * feat: add postgres db to github actions
    
    * fix: add DATABASE_URL env variable to GitHub actions

---
+++ src/prisma/prisma.service.spec.ts
@@ -0,0 +1,30 @@
+import type { TestingModule } from "@nestjs/testing";
+import { Test } from "@nestjs/testing";
+
+import { PrismaService } from "./prisma.service";
+
+describe("PrismaService", () => {
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService],
+    }).compile();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    await prisma.onModuleInit();
+  });
+
+  afterAll(async () => {
+    await prisma.$disconnect();
+  });
+
+  it("should be defined", () => {
+    expect(prisma).toBeDefined();
+  });
+
+  it("should connect to the database", async () => {
+    const result = await prisma.$queryRaw`SELECT 1`;
+    expect(result).toBeDefined();
+  });
+});
