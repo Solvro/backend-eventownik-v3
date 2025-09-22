b336274598390742932b11be61db6c91e430fc3a
PB3D <pb3d.dev>
2025-09-22 12:52:00 +0200

    feat: auth token guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 
 import { AppService } from "./app.service";
+import { PrismaService } from "./prisma/prisma.service";
+import { TokenV2Guard } from "./token-v2/token-v2.guard";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
+  @UseGuards(TokenV2Guard)
   getHello(): string {
     return this.appService.getHello();
   }
