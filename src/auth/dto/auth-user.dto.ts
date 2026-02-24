cda47ab3e8544d4f1e7fa6e3f981465e1a887cc3
loudsheep <loudsheep05@gmail.com>
2026-02-24 13:52:37 +0100

    feat: swagger docs, permissions dto

--- src/auth/dto/auth-user.dto.ts
+++ src/auth/dto/auth-user.dto.ts
@@ -2,8 +2,6 @@ import { Admin, OrganizerType } from "src/generated/prisma/client";
 
 import { ApiProperty } from "@nestjs/swagger";
 
-import { PermissionDto } from "./permission.dto";
-
 export class AdminDto implements Omit<Admin, "password"> {
   @ApiProperty({ example: "123e4567-e89b-12d3-a456-426614174000" })
   uuid!: string;
@@ -28,9 +26,4 @@ export class AdminDto implements Omit<Admin, "password"> {
 
   @ApiProperty({ example: "2023-01-01T00:00:00.000Z" })
   updatedAt!: Date;
-}
-
-export class AuthUserDto extends AdminDto {
-  @ApiProperty({ type: PermissionDto, isArray: true })
-  permissions!: PermissionDto[];
 }
