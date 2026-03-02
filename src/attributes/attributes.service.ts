eefebce4fbb0deb35341861a7d7f87ce8eb35595
Kajta <kajtakamil1@gmail.com>
2026-03-02 13:57:36 +0100

    feat: attributes-module delete method

--- src/attributes/attributes.service.ts
+++ src/attributes/attributes.service.ts
@@ -80,7 +80,22 @@ export class AttributesService {
     });
   }
 
-  // remove(id: number) {
-  //   return `This action removes a #${id} attribute`;
-  // }
+  async remove(id: string, eventId: string) {
+    const event = await this.prisma.event.findUnique({
+      where: { uuid: eventId },
+      include: { attributes: true },
+    });
+    if (event == null) {
+      throw new NotFoundException("Event not found");
+    }
+    const foundAttribute = event.attributes.find(
+      (attribute) => attribute.uuid === id,
+    );
+    if (foundAttribute == null) {
+      throw new NotFoundException("Attribute not found");
+    }
+    return this.prisma.attribute.delete({
+      where: { uuid: id },
+    });
+  }
 }
